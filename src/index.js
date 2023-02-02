e39a87c4b66a63420d46ce3713923c0f7aefc8f4
artyomakulov <88495795+artyomakulov@users.noreply.github.com>
2023-02-02 20:03:40 +0200

    пробую дальше
    пробую дальше

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// import ReactDOM from 'react-dom';
+
+const elem1 = <span>zalupa</span>
+const elem2 = <span>ssanaya</span>
+
+const element = <div>{elem1}{elem2}</div>
+
+console.log(element)
+
+
+// ReactDOM.render(element, document.querySelector('#root'))
+
+////////////////////////////////////
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { App } from 'components/App';
+// import './index.css';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
